16c247b53d4ed92a1ff3c6da0641735a40c9dce6
Bennani Soufiane <soufianeben94@gmail.com>
2019-01-26 15:22:21 +0100

    Generate users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(), UsersModule],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
 })
 
 export class AppModule {  
